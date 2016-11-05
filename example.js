40ecf05ac67e1d1ac506170394b9d28234d3e5c1
HuangJi <a1345962293@gmail.com>
2016-11-05 13:19:04 +0800

    Fix 8btc bug and updated to v0.2.14

--- example.js
+++ example.js
@@ -1,8 +1,12 @@
-var btcnews = require('./btcnews');
+const btcnews = require('./btcnews');
 
 // getPosts(source, callback), and source could use technews, bnext, btclub, 8btc
-btcnews.getPosts('coindesk', function(err, body) {
+btcnews.getPosts('8btc', (err, body) => {
+  if (err) {
+    console.error(err);
+  } else {
     console.log(body);
+  }
 });
 // console.log(new Date('2016-05-03T13:32:11+00:00') / 1000)
 // console.log(btcnews.sourceList);
